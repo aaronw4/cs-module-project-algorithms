07ce2910abd26fc3ecd1cf54a8faea532c008361
Aaron Williams <aaronrw4@yahoo.com>
2020-08-11 20:02:43 -0700

    Wrote a 2nd solution for sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,18 +2,29 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
-def sliding_window_max(nums, k):
-    # Your code here
-    index = k - 1
+#Recursive
+# def sliding_window_max(nums, k):
+#     # Your code here
+#     index = k - 1
 
-    if len(nums) == k:
-        nums.sort()
-        return [nums[index]]
-    else:        
+#     if len(nums) == k:
+#         nums.sort()
+#         return [nums[index]]
+#     else:        
+#         temp = nums[:k]
+#         temp.sort()
+#         return [temp[index]] + sliding_window_max(nums[1:], k)
+
+#Iterative
+def sliding_window_max(nums, k):
+    arr = []
+    
+    while len(nums) >= k:
         temp = nums[:k]
         temp.sort()
-        return [temp[index]] + sliding_window_max(nums[1:], k)
-
+        arr.append(temp[k-1])
+        nums.pop(0)
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
