ab8099898324223f581c86a597b6b8e60f613a73
Aaron Williams <aaronrw4@yahoo.com>
2020-08-11 19:43:20 -0700

    Finished sliding window max for reg test

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,12 +4,20 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    index = k - 1
 
-    pass
+    if len(nums) == k:
+        nums.sort()
+        return [nums[index]]
+    else:        
+        temp = nums[:k]
+        temp.sort()
+        return [temp[index]] + sliding_window_max(nums[1:], k)
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
+    # arr = [1, 3, 2]
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
