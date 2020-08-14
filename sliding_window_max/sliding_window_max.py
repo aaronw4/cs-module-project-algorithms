7bc3b7f9ec14ea5e450b66a3d260623a60e579ce
Aaron Williams <aaronrw4@yahoo.com>
2020-08-13 18:14:15 -0700

    Used cache to pass large number test

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -3,28 +3,33 @@ Input: a List of integers as well as an integer `k` representing the size of the
 Returns: a List of integers
 '''
 #Recursive
-# def sliding_window_max(nums, k):
-#     # Your code here
-#     index = k - 1
+def sliding_window_max(nums, k, cache={}):
+    # Your code here
+    index = k - 1
 
-#     if len(nums) == k:
-#         nums.sort()
-#         return [nums[index]]
-#     else:        
-#         temp = nums[:k]
-#         temp.sort()
-#         return [temp[index]] + sliding_window_max(nums[1:], k)
-
-#Iterative
-def sliding_window_max(nums, k):
-    arr = []
-    
-    while len(nums) >= k:
+    if len(nums) == k:
+        nums.sort()
+        return [nums[index]]
+    elif k in cache:
+        return cache[k]
+    else:        
         temp = nums[:k]
         temp.sort()
-        arr.append(temp[k-1])
-        nums.pop(0)
-    return arr
+        x = [temp[index]] + sliding_window_max(nums[1:], k, cache)
+        cache[k] = x
+        return x
+
+#Iterative
+# def sliding_window_max(nums, k):
+#     arr = []
+    
+#     while len(nums) >= k:
+#         temp = nums[:k]
+#         temp.sort()
+#         arr.append(temp[k-1])
+#         nums.pop(0)
+#     return arr
+
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
